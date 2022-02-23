01b94e03f01f5b74cc5c3447331c773834b92b0b
krayf <freestonekray@gmail.com>
2022-02-23 08:15:23 -0500

    another attempted fix

---
+++ api/routes.py
@@ -0,0 +1,13 @@
+import time
+from api import app
+from flask import send_from_directory
+
+# root, serving our react app
+@app.route("/")
+def root():
+    return 'hi'
+    return app.send_static_file('index.html')
+# ----------------------------- back-end routes --------------------------------
+@app.route('/api/time')
+def api_time():
+    return {'time': time.time()}
