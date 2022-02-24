75da6b57894db7b1ff9536b2f1644b88c9f26c7e
krayf <freestonekray@gmail.com>
2022-02-23 19:51:11 -0500

    apparently this works, reformmating

--- api/routes.py
+++ api/routes.py
@@ -2,21 +2,14 @@ import time
 from api import app
 from flask import send_from_directory
 
+# routes have to match the front end, no idea why or how to avoid
 # root, serving our react app
 @app.route("/")
-def root():
-    return app.send_static_file('index.html')
-
-# lets users get to other routes (controlled in the frontend react app)
-# @app.route('/')
-# @app.route('/<first>')
-# @app.route('/<first>/<path:rest>')
-# def fallback(first=None, rest=None):
-#     return app.send_static_file('index.html')
+def root(): return app.send_static_file('index.html')
 
 @app.route("/geog777-proj1")
-def geo():
-    return app.send_static_file('index.html')
+def geog777_proj1(): return app.send_static_file('index.html')
+
 # ----------------------------- back-end routes --------------------------------
 @app.route('/api/time')
 def api_time():
