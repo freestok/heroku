fe8c450b7724d6a96380f28880779be3f76edcf4
krayf <freestonekray@gmail.com>
2022-02-23 19:44:55 -0500

    trying to make routing work in heroku

--- api/routes.py
+++ api/routes.py
@@ -8,12 +8,15 @@ def root():
     return app.send_static_file('index.html')
 
 # lets users get to other routes (controlled in the frontend react app)
-@app.route('/')
-@app.route('/<first>')
-@app.route('/<first>/<path:rest>')
-def fallback(first=None, rest=None):
-    return app.send_static_file('index.html')
+# @app.route('/')
+# @app.route('/<first>')
+# @app.route('/<first>/<path:rest>')
+# def fallback(first=None, rest=None):
+#     return app.send_static_file('index.html')
 
+@app.route("/geog777-proj1")
+def geo():
+    return app.send_static_file('index.html')
 # ----------------------------- back-end routes --------------------------------
 @app.route('/api/time')
 def api_time():
